b35b71b9dca36bee73e383cdcee5652931c512ff
prakirth govardhanam <prakigov@student.uef.fi>
2026-04-28 17:10:51 +0300

    tests: Validate user login, update and get info endpoints

--- backend/app/routes/test_auth.py
+++ backend/app/routes/test_auth.py
@@ -4,15 +4,14 @@ from app.db.session import SessionLocal
 from app.models import User
 from app.main import app
 
-
+# setup test user
 test_email = "test.user@example.com"
 test_pwd = "testuser123"
 test_name = "test user 1"
-
-#client = TestClient(app)
+update_test_name = "Master Shifu"
 
 # teardown test inputs committed to production database
-@pytest.fixture
+@pytest.fixture(scope="module")
 def client():
   with TestClient(app) as test_client:
     yield test_client
@@ -58,3 +57,42 @@ def test_new_professional_signup(client):
   assert "password" not in json
 
 
+def test_valid_login(client):
+  response = client.post(
+    "auth/login",
+    json= {
+      "email": test_email,
+      "password": test_pwd
+    }
+  )
+  assert response.status_code == 200
+  json = response.json()
+  token = json["access_token"]
+  assert token is not None
+  assert json["token_type"] == "bearer"
+  assert json["user"]["email"] == test_email
+  assert json["user"]["role"] == "professional"
+  return token
+
+def test_get_valid_user_info(client):
+  token = test_valid_login(client)
+  response = client.get(
+    "auth/me",
+    headers={ "Authorization":f"Bearer {token}" }
+  )
+  assert response.status_code == 200
+  json = response.json()
+  assert json["email"] == test_email
+  assert json["full_name"] == test_name
+
+def test_update_valid_user_info(client):
+  token = test_valid_login(client)
+  response = client.patch(
+    "auth/me",
+    headers={ "Authorization":f"Bearer {token}"},
+    json={"full_name": update_test_name}
+  )
+  assert response.status_code == 200
+  json = response.json()
+  assert json["email"] == test_email
+  assert json["full_name"] == update_test_name
