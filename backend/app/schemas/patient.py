d4e1b9c62f9f1a8cd176df302d72124a69d173f7
prakirth govardhanam <prakigov@student.uef.fi>
2026-04-30 16:53:37 +0300

    docs: Add comments on patient endpoints and schemas

--- backend/app/schemas/patient.py
+++ backend/app/schemas/patient.py
@@ -1,12 +1,12 @@
 from pydantic import BaseModel, EmailStr
 
-
+# Pydantic Data Model used to define the structure and validation of data for patient registration by registered professional
 class CreatePatientRequest(BaseModel):
     email: EmailStr
     password: str
     full_name: str
 
-
+# Pydantic Data Model used to define structure of response returned after successful patient creation.
 class PatientResponse(BaseModel):
     id: str
     email: str
