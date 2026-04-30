69d9a1b9bd261d56e6d3211a99260cd447ccfe2b
prakirth govardhanam <prakigov@student.uef.fi>
2026-04-30 17:16:23 +0300

    refactor: Simplify teardown comments in test client fixture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -11,20 +11,18 @@ test_email = "test.user@example.com"
 test_professional_email = "prof.test@example.com"
 test_patient_email = "patient.test@example.com"
 
-# teardown test inputs committed to database by test_auth
+# teardown test inputs committed to database
 @pytest.fixture(scope="module")
 def client():
   with TestClient(app) as test_client:
     yield test_client
     db = SessionLocal()
     try:
+      # teardown test inputs committed to database by test_auth 
       db.query(User).filter(User.email.in_([test_email])).delete(synchronize_session=False)
       db.commit()
-    finally:
-      db.close()
 
     # teardown test inputs committed to database by test_patients
-    try:
       db.query(PatientProfessionalLink).filter(
             PatientProfessionalLink.professional_id.in_(
               db.query(User).filter(User.email.in_([test_professional_email])).with_entities(User.id))
